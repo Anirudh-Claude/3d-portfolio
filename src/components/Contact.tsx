5c9c915641c5d7cdf8b2fcd678714589e3dcf57a
Anirudh Koppuravuri <221583116+Anirudh01-os@users.noreply.github.com>
2026-04-23 23:05:42 +0530

    Contact

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -11,7 +11,7 @@ const Contact = () => {
             <h4>Connect</h4>
             <p>
               <a
-                href="https://www.linkedin.com/in/akashrmalhotra/"
+                href="https://www.linkedin.com/in/anirudh-koppuravuri-762643338/"
                 target="_blank"
                 rel="noreferrer"
                 data-cursor="disable"
@@ -21,17 +21,17 @@ const Contact = () => {
             </p>
             <h4>Education</h4>
             <p>
-              PGPWE (MBA), Indian Institute of Management, Lucknow — 2021–2023
+              CSE (B.Tech), Vellore Institute of Technology, Vellore — 2024–2028
             </p>
             <p>
-              B.Tech Computer Science, Manav Rachna Educational Institutions —
-              2009–2013
+              Intermediate, Narayana Educational Institutions —
+              2022–2024
             </p>
           </div>
           <div className="contact-box">
             <h4>Social</h4>
             <a
-              href="https://github.com/akashrmalhotra"
+              href="https://github.com/Anirudh01-os"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -40,7 +40,7 @@ const Contact = () => {
               GitHub <MdArrowOutward />
             </a>
             <a
-              href="https://www.linkedin.com/in/akashrmalhotra/"
+              href="https://www.linkedin.com/in/anirudh-koppuravuri-762643338/"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -49,16 +49,16 @@ const Contact = () => {
               LinkedIn <MdArrowOutward />
             </a>
             <a
-              href="https://www.youtube.com/@Leftbraincoder"
+              href="anirudhkoppuravuri1@gmail.com"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
               className="contact-social"
             >
-              YouTube <MdArrowOutward />
+              E-Mail <MdArrowOutward />
             </a>
             <a
-              href="https://www.instagram.com/leftbraincoder/"
+              href="https://www.instagram.com/_.urstrulyanirudh._/"
               target="_blank"
               rel="noreferrer"
               data-cursor="disable"
@@ -69,7 +69,7 @@ const Contact = () => {
           </div>
           <div className="contact-box">
             <h2>
-              Designed and Developed <br /> by <span>Akash Malhotra</span>
+              Designed and Developed <br /> by <span>Anirudh Koppuravuri</span>
             </h2>
             <h5>
               <MdCopyright /> 2026
