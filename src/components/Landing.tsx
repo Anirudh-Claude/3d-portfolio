a6987aad532d9bdd782dba598714fea109f9b85d
Anirudh Koppuravuri <221583116+Anirudh01-os@users.noreply.github.com>
2026-04-23 23:09:12 +0530

    Anirudh name

--- src/components/Landing.tsx
+++ src/components/Landing.tsx
@@ -9,20 +9,20 @@ const Landing = ({ children }: PropsWithChildren) => {
           <div className="landing-intro">
             <h2>Hello! I'm</h2>
             <h1>
-              AKASH
+              ANIRUDH
               <br />
-              <span>MALHOTRA</span>
+              <span>Anirudh</span>
             </h1>
           </div>
           <div className="landing-info">
-            <h3>Co-Founder &</h3>
+            <h3>Founder &</h3>
             <h2 className="landing-info-h2">
-              <div className="landing-h2-1">Tech</div>
-              <div className="landing-h2-2">Business</div>
+              <div className="landing-h2-1">Nexus Labs</div>
+              <div className="landing-h2-2">Treads</div>
             </h2>
             <h2>
-              <div className="landing-h2-info">Business</div>
-              <div className="landing-h2-info-1">Tech</div>
+              <div className="landing-h2-info">AI Engineer</div>
+              <div className="landing-h2-info-1">MERN Stack</div>
             </h2>
           </div>
         </div>
